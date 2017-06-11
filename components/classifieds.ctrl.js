13e14b419a5249d8f3eb7111fe8dba77fe25d2b8
Joe Saraceno <honestcomrade@gmail.com>
2017-06-10 20:54:52 -0700

    filters

--- components/classifieds.ctrl.js
+++ components/classifieds.ctrl.js
@@ -3,7 +3,7 @@
 
   angular
     .module("ngClassifieds")
-    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
+    .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
 
       classifiedsFactory.getClassifieds().then(function(classifieds) {
         $scope.classifieds = classifieds.data;
@@ -29,19 +29,47 @@
           $scope.classifieds.push(classified);
           $scope.classified = {};
           $scope.closeSidebar();
-          $mdToast.show(
-            $mdToast.simple()
-              .content("Classified Saved!")
-              .position('top, right')
-              .hideDelay(3000)
-          );
+          showToast("Classified Saved");
         }
       }
 
       $scope.editClassified = function(classified) {
         $scope.editing = true;
         $scope.openSidebar();
+        $scope.classified = classified;
       }
+
+      $scope.saveEdit = function() {
+        $scope.editing = false;
+        $scope.classified = {};
+        $scope.closeSidebar();
+        showToast("Edit Saved!");
+      }
+
+      $scope.deleteClassified = function(event, classified) {
+        var confirm = $mdDialog.confirm()
+          .title('Are you sure you want to delete ' + classified.title + '?')
+          .ok('Yes')
+          .cancel('No')
+          .targetEvent(event);
+        $mdDialog.show(confirm).then(function() {
+          var index = $scope.classifieds.indexOf(classified);
+          $scope.classifieds.splice(index, 1);
+          showToast(classified.title + " deleted");
+        }, function () {
+
+        });
+      }
+
+      function showToast(message) {
+        $mdToast.show(
+          $mdToast.simple()
+            .content(message)
+            .position('top, right')
+            .hideDelay(3000)
+        );
+      }
+
       
     });
 })();
