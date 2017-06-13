19560e4788b7f84717f75907deb8ffbf6ad45619
Joe Saraceno <honestcomrade@gmail.com>
2017-06-13 07:33:42 -0700

    change

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,18 @@
 angular
-  .module("ngClassifieds", ["ngMaterial"])
-  .config(function($mdThemingProvider) {
+  .module("ngClassifieds", ["ngMaterial", 'ui.router'])
+  .config(function($mdThemingProvider, $stateProvider) {
+    
     $mdThemingProvider.theme('default')
       .primaryPalette('teal')
       .accentPalette('orange');
 
+    $stateProvider
+      .state('stateone', {
+        url: '/stateone',
+        template: '<h1>State One</h1>'
+      })
+      .state('statetwo', {
+        url: '/statetwo',
+        template: '<h1>State Two</h1>'
+      });
   });
