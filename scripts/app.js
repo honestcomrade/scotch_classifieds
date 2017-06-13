db6523cab25eb69761ecc65f8321165691c0b0e3
Joe Saraceno <honestcomrade@gmail.com>
2017-06-13 07:50:58 -0700

    state-ctrl

--- scripts/app.js
+++ scripts/app.js
@@ -9,10 +9,14 @@ angular
     $stateProvider
       .state('stateone', {
         url: '/stateone',
-        template: '<h1>State One</h1>'
+        template: '<h1>{{ stateone.message }}</h1>',
+        controller: 'stateOneCtrl as stateone'
       })
       .state('statetwo', {
         url: '/statetwo',
         template: '<h1>State Two</h1>'
       });
+  })
+  .controller('stateOneCtrl', function($scope) {
+    this.message = "Hey from state one";
   });
